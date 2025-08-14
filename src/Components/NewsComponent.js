44361777ad7a7055917a7591617c0a37e60a8f75
Aakshat-01 <aakshatgupta40@gmail.com>
2025-08-14 15:06:24 +0530

    Newsapp using classBases Components

---
+++ src/Components/NewsComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsComponent extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default NewsComponent
